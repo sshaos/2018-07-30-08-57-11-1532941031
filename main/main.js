90867c1ed6f619eb7075f39623062db1d8edb72a
shaoshuai <shaoshuai@hust.edu.cn>
2018-08-02 17:02:24 +0800

    add main

--- main/main.js
+++ main/main.js
@@ -1,4 +1,43 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+    //检测非法输入，str应该为0-9之间组合的数字
+    var reg = /^[0-9]*[0-9][0-9]*$/;
+    if(!reg.test(str))
+    {
+        console.error("Please check your input!\n");
+        return;
+    }
+    let lcd_num = new Lcd_num(str);
+    return lcd_num.print_Lcd().join("");
+};
+class Lcd_num{
+    constructor(input){
+        this.number = input;
+        this.size = input.length;
+    }
+    //打印函数：将输入的字符串打印成Lcd
+    print_Lcd(){
+        var lcd = [];
+        for(var row = 0; row < 3; row++ )
+        {
+            for(var i = 0; i < this.size ;i++)
+          {
+            lcd.push(Lcd_scr[this.number[i]][row]);
+            (i == this.size-1)?lcd.push('\n'):lcd.push(' ');
+          }
+        }
+        return lcd;
+    }
+};
+//定义常量Lcd_scr存放lcd数字信息，不可更改
+const Lcd_scr = {
+    '0' : ['._.','|.|','|_|'],
+    '1' : ['...','..|','..|'],
+    '2' : ['._.','._|','|_.'],
+    '3' : ['._.','._|','._|'],
+    '4' : ['...','|_|','..|'],
+    '5' : ['._.','|_.','._|'],
+    '6' : ['._.','|_.','|_|'],
+    '7' : ['._.','..|','..|'],
+    '8' : ['._.','|_|','|_|'],
+    '9' : ['._.','|_|','..|'],
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,7 +1,6 @@
 const main = require('../main/main');
 
-describe('main()', () => {
-
+describe('main()', () => {  
     it('should return 910', () => {
         let actual = main("910");
         expect(actual).toBe(
@@ -9,6 +8,7 @@ describe('main()', () => {
             "|_| ..| |.|\n"+
             "..| ..| |_|\n");
     });
+    
 
     it('should return 256', () => {
         let actual = main("256");
@@ -25,6 +25,36 @@ describe('main()', () => {
             "._.\n"+
             "..|\n"+
             "..|\n");
+    });
+
+    //判断字库是否有问题
+    it('should return 9876543210', () => {
+        let actual = main("9876543210");
+        expect(actual).toBe(
+            "._. ._. ._. ._. ._. ... ._. ._. ... ._.\n"+
+            "|_| |_| ..| |_. |_. |_| ._| ._| ..| |.|\n"+
+            "..| |_| ..| |_| ._| ..| ._| |_. ..| |_|\n");
+    });
+
+    //输入非法的情况
+    it('should return error', () => {
+        let actual = main("3.12");
+        expect(actual).toBe(undefined);
+    });
+
+    it('should return error', () => {
+        let actual = main("-2");
+        expect(actual).toBe(undefined);
+    });
+
+    it('should return error', () => {
+        let actual = main("1@4");
+        expect(actual).toBe(undefined);
+    });
+
+    it('should return error', () => {
+        let actual = main("4abc");
+        expect(actual).toBe(undefined);
     });
 
 });
